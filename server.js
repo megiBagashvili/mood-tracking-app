488f51870e4817d9b47a8b547fefd4b187189c73
megiBagashvili <megibagashvili2004@gmail.com>
2025-11-09 22:22:08 +0400

    cors

--- server.js
+++ server.js
@@ -2,25 +2,40 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const cors = require('cors');
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
 
 app.use(express.json());
 
+const whitelist = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  process.env.FRONTEND_URL,
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 app.get('/', (req, res) => {
   res.send('Welcome to the Mood Tracker API!');
 });
 
 app.use('/api/auth', require('./routes/auth'));
-
 app.use('/api/users', require('./routes/user'));
-
 app.use('/api/moods', require('./routes/moods'));
-
 app.use('/api/dashboard', require('./routes/dashboard'));
 
 app.use(notFound);
